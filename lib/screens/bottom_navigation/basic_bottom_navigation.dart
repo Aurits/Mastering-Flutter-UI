4d3fc6886b4d043181819552682c0cf3b0effda6
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-19 16:41:15 +0000

    Nov 19, 2023, 7:41 PM

---
+++ lib/screens/bottom_navigation/basic_bottom_navigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BasicBottomNavigation extends StatefulWidget {
+  const BasicBottomNavigation();
+
+  @override
+  State<BasicBottomNavigation> createState() => _BasicBottomNavigationState();
+}
+
+class _BasicBottomNavigationState extends State<BasicBottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
