4d1a5beb3291d1b02028eeccd445f10e034e920e
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-19 17:04:26 +0000

    Nov 19, 2023, 8:04 PM

--- lib/screens/bottom_navigation/basic_bottom_navigation.dart
+++ lib/screens/bottom_navigation/basic_bottom_navigation.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ui/model/bottom_nav.dart';
 
 class BasicBottomNavigation extends StatefulWidget {
-  final List<BottomNav> items = <BottomNav>[
-    BottomNav('Home', Icons.store, null),
-    BottomNav('Business', Icons.business, null),
-    BottomNav('Profile', Icons.account_circle, null),
-  ];
-  BasicBottomNavigation();
+  const BasicBottomNavigation({Key? key}) : super(key: key);
 
   @override
   State<BasicBottomNavigation> createState() => _BasicBottomNavigationState();
 }
 
 class _BasicBottomNavigationState extends State<BasicBottomNavigation> {
+  final List<BottomNav> itemsNav = <BottomNav>[
+    BottomNav('Home', Icons.store, null),
+    BottomNav('Business', Icons.business, null),
+    BottomNav('Shop', Icons.shopping_cart, null),
+    BottomNav('Profile', Icons.account_circle, null),
+  ];
   int currentIndex = 0;
   late BuildContext ctx;
 
@@ -25,6 +26,17 @@ class _BasicBottomNavigationState extends State<BasicBottomNavigation> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: itemsNav.map(
+          (BottomNav d) {
+            return BottomNavigationBarItem(
+              icon: Icon(d.icon),
+              label: d.title,
+            );
+          },
+        ).toList(),
+      ),
+    );
   }
 }
