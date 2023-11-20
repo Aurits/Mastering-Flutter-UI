7071c9b979ef36b2182fc74b982dd0c059e2f77b
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-20 10:48:27 +0000

    Nov 20, 2023, 1:48 PM

---
+++ lib/screens/Cards/basic_cards.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BasicCards extends StatefulWidget {
+  const BasicCards();
+
+  @override
+  State<BasicCards> createState() => _BasicCardsState();
+}
+
+class _BasicCardsState extends State<BasicCards> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
