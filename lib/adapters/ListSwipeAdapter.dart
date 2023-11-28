6a05d3c4baf5651b5bafc2c8159aac2ec5931971
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-28 07:12:46 +0000

    Nov 28, 2023, 10:12 AM

---
+++ lib/adapters/ListSwipeAdapter.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/data/my_colors.dart';
+
+import '../model/people.dart';
+
+class ListSwipeAdapter {
+  List? items = <People>[];
+  List itemsTile = <ItemTile>[];
+  Function onReorder;
+
+  ListSwipeAdapter(this.items, this.onReorder);
+
+  Widget getView() {
+    return ReorderableListView(
+      onReorder: _onReorder,
+      scrollDirection: Axis.vertical,
+      children: List.generate(this.items!.length, (index) {
+        return ItemTile(
+          key: Key('$index'),
+          index: index,
+          object: this.items![index],
+        );
+      }),
+    );
+  }
+
+  void _onReorder(int oldIndex, int newIndex) {
+    if (newIndex > oldIndex) newIndex -= 1;
+
+    final People item = this.items!.removeAt(oldIndex);
+    this.items!.insert(newIndex, item);
+    this.onReorder();
+  }
+}
+
+class ItemTile extends StatefulWidget {
+  final int index;
+  final People object;
+  final Key key;
+
+  const ItemTile({
+    required this.key,
+    required this.index,
+    required this.object,
+  });
+
+  @override
+  State<ItemTile> createState() => _ItemTileState();
+}
+
+class _ItemTileState extends State<ItemTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListTile(
+          key: PageStorageKey<int>(widget.index),
+          leading: Container(
+            child: CircleAvatar(
+              backgroundImage: AssetImage(
+                widget.object.image,
+              ),
+            ),
+            width: 50,
+            height: 50,
+          ),
+          title: Text(
+            widget.object.name!,
+            style: TextStyle(
+              color: Colors.grey[880],
+            ),
+          ),
+          trailing: Icon(
+            Icons.drag_handle,
+            color: MyColors.grey_40,
+          ),
+        ),
+        Divider(
+          height: 0,
+        )
+      ],
+    );
+  }
+}
---
+++ lib/screens/lists/swipe_list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/adapters/ListDragAdapter.dart';
+import 'package:flutter_ui/data/dummy.dart';
+import 'package:flutter_ui/model/people.dart';
+import 'package:flutter_ui/widget/toolbar.dart';
+
+class SwipeLists extends StatefulWidget {
+  const SwipeLists();
+
+  @override
+  State<SwipeLists> createState() => _SwipeListsState();
+}
+
+class _SwipeListsState extends State<SwipeLists> {
+  late BuildContext context;
+  List<People>? items;
+
+  @override
+  void initState() {
+    super.initState();
+    items = Dummy.getPeopleData();
+  }
+
+  void onReorder() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+
+    return Scaffold(
+      appBar: CommonAppBar.getPrimaryAppbar(context, "Swipe Lists")
+          as PreferredSizeWidget?,
+      body: ListDragAdapter(items, onReorder).getView(),
+    );
+  }
+}
