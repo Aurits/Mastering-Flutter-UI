6fa5a696f5d0ea7213d019ac08ca5e2b14d94301
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-28 02:46:14 +0000

    Nov 28, 2023, 5:46 AM

---
+++ lib/screens/lists/basic_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BasicLists extends StatefulWidget {
+  const BasicLists();
+
+  @override
+  State<BasicLists> createState() => _BasicListsState();
+}
+
+class _BasicListsState extends State<BasicLists> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
